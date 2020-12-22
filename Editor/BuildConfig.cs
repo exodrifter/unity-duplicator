98c5134851eec46f8630358f3bf820e4bbeea916
Ava Pek <code@exodrifter.space>
2020-12-22 00:15:35 -0600

    Add files from Anchor
    Extracted the relevant files from the Anchor project needed for the
    build tool.

---
+++ Editor/BuildConfig.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using UnityEditor;
+
+namespace Exodrifter.Anchor.Editor
+{
+	[Serializable]
+	public class BuildConfig
+	{
+		public string folder;
+		public string exeName;
+		public bool defaultBuild;
+		public BuildTarget target;
+		public BuildOptions options;
+	}
+}
